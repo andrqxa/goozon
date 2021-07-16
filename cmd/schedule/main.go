9214dac56090387e91e8004cda66ed89794faa18
Andrii Puhachenko <ext-andrii.puhachenko@here.com>
2021-07-16 17:14:06 +0300

    Fixes in main

--- cmd/schedule/main.go
+++ cmd/schedule/main.go
@@ -7,19 +7,6 @@ import (
 	"github.com/andrqxa/goozon/model"
 )
 
-var (
-	unorderTestDate = `16.07.2021 09:00:00; 16.07.2021 09:30:00
-	16.07.2021 08:15:00; 16.07.2021 08:45:00
-	16.07.2021 08:00:00; 16.07.2021 08:30:00
-	16.07.2021 08:40:00; 16.07.2021 09:00:00
-	`
-	orderTestDate model.ParkingSeq
-)
-
-const (
-	timeTemplate = "02.01.2006 15:04:05"
-)
-
 func main() {
 	parkSeq, err := model.GetParkingSequence(os.Stdin)
 	if err != nil {
